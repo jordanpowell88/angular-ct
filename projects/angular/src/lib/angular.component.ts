46e47e4dda3a14342467e4bf03bd665b3759b7ae
Jordan <jordan@jpdesigning.com>
2022-02-10 13:19:24 -0500

    chore: generate angular library

---
+++ projects/angular/src/lib/angular.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-angular',
+  template: `
+    <p>
+      angular works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class AngularComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
