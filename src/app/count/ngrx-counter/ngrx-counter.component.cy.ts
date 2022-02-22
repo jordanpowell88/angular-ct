583589938783416ad743e08735ce5ad4af1a9b6e
Jordan <jordan@jpdesigning.com>
2022-02-22 10:19:12 -0500

    feat(angular): add overrides property to config

--- src/app/count/ngrx-counter/ngrx-counter.component.cy.ts
+++ src/app/count/ngrx-counter/ngrx-counter.component.cy.ts
@@ -1,13 +1,27 @@
+import 'zone.js';
+import 'zone.js/testing';
 import { provideMockStore } from "@ngrx/store/testing"
 import { mount } from "../../../../projects/angular/src/public-api"
 import { NgrxCounterComponent } from "./ngrx-counter.component"
+import { initialState } from '../count-store/count.reducer';
 
 describe('NgRxCounterComponent', () => {
     beforeEach(() => {
-        mount(NgrxCounterComponent, { providers: [provideMockStore({ })]})
+        mount(NgrxCounterComponent, {
+            overrides: {
+                set: { 
+                    providers: [provideMockStore()]
+                }
+            }
+        })
     })
-
     it('can mount', () => {
         cy.get('h3').contains('NgRx Counter: 0');
+    })
+
+    it('can increment the count by dispatching action and selectors updates', () => {
+        cy.get('h3').contains('NgRx Counter: 0');
+        cy.get('button').contains('Increment +').click();
+        cy.get('h3').contains('NgRx Counter: 1');
     })
 })
