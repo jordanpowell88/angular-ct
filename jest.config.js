58f4fa2454ac28c92d4cebc9781818810c573a67
Jordan <jordan@jpdesigning.com>
2022-06-13 13:42:05 -0400

    chore: remove jest old jest config

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,3 @@
-// module.exports = {
-//   moduleNameMapper: {
-//     '@core/(.*)': '<rootDir>/src/app/core/$1',
-//   },
-//   resolver: 'jest-preset-angular/build/resolvers/ng-jest-resolver.js',
-//   transformIgnorePatterns: ['node_modules/(?!@angular)', 'node_modules/(?!(jest-test|@ngrx))'],
-//   transform: {
-//     '^.+\\.(ts|js|mjs|html|svg)$': 'jest-preset-angular',
-//   },
-// };
-
-
 module.exports = {
   preset: 'jest-preset-angular',
 };
